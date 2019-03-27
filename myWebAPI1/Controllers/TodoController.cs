f2ba649d833e73ad78aa6333d2e8d617730adb75
Xiaoli Mao <ntmxl5288@gmail.com>
2019-03-27 07:35:40 -0500

    Try Web API

---
+++ myWebAPI1/Controllers/TodoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using myWebAPI1.Models;
+
+namespace myWebAPI1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly ToDoContext _context;
+        
+        public TodoController(ToDoContext context)
+        {
+            _context = context;
+
+            if(_context.ToDoItems.Count() == 0)
+            {
+                _context.ToDoItems.Add(new ToDoItem { Name = "item1" });
+                _context.SaveChanges();
+            }
+        }
+
+        // GET: api/ToDo
+        [HttpGet]
+        public async Task<ActionResult<ToDoItem>> GetTodoItem(long id)
+        {
+            var todoItem = await _context.ToDoItems.FindAsync(id);
+            if(todoItem == null)
+            {
+                return NotFound();
+            }
+            return todoItem;
+        }
+    }
+}
--- myWebAPI1/Startup.cs
+++ myWebAPI1/Startup.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+//using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+//using Microsoft.Extensions.Logging;
+//using Microsoft.Extensions.Options;
+using myWebAPI1.Models;
 
 namespace myWebAPI1
 {
@@ -25,6 +27,8 @@ namespace myWebAPI1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ToDoContext>(opt =>
+            opt.UseInMemoryDatabase("Todolist"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
